ff427a3bb46a09f85922f08075faedbf456fd05f
EliteBook <kylehensman@gmail.com>
2019-03-13 14:43:54 -0300

    fixed react app and tested the firebase deploy

---
+++ app/build/precache-manifest.e4f0f212460006f1d26bf77a0844058b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d96ca77522359629e492",
+    "url": "/static/css/main.710a1ece.chunk.css"
+  },
+  {
+    "revision": "d96ca77522359629e492",
+    "url": "/static/js/main.ca77ce55.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ba264cd90c4d36546d5a",
+    "url": "/static/js/2.8f71a45e.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "3ae6b396c9ff60faf425dd8a48eb954d",
+    "url": "/index.html"
+  }
+];
